fe6f4eac7a7000077809472b060c3257e4d82846
producaoinove <producao@inoveempresarial.com.br>
2023-11-07 17:40:24 -0300

    insercao de usuarios

--- assets/script/scriptlogin.js
+++ assets/script/scriptlogin.js
@@ -4,7 +4,7 @@ function realizarLOGIN() {
         event.preventDefault();
         const username = form.username.value;
         const password = form.password.value;
-        if ((username === "higor.ruy" && password === "123456") || (username === "producao.inove" && password === "123456") || (username === "jhenyfer.cordeiro" && password === "123456")) {
+        if ((username === "higor.ruy" && password === "123456") || (username === "producao.inove" && password === "123456") || (username === "jhenyfer.cordeiro" && password === "123456") || (username === "vendas.inove" && password === "123456") || (username === "back.office" && password === "123456")) {
             alert('SUCESSO NO LOGIN. PRESSIONE OK PARA CONTINUAR!');
             window.location.href = "private/home.html";
         } else {
