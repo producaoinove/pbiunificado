02ff3136225aec2ab3e3129cb78c26c8407b0f61
producaoinove <producao@inoveempresarial.com.br>
2023-11-13 16:42:34 -0300

    atualizacao do dia 13.11

--- assets/script/scriptvalidador.js
+++ assets/script/scriptvalidador.js
@@ -1,6 +1,7 @@
-if (localStorage.getItem('primeiroAcesso') === null) {
-    if (window.location.pathname !== '../index.html') {
-        window.location.href = '../index.html';
-    }
-    localStorage.setItem('primeiroAcesso', 'true');
-}
+// if (localStorage.getItem('primeiroAcesso') === null) {
+//     if (window.location.pathname !== '../index.html') {
+//         window.location.href = '../index.html';
+//     }
+//     localStorage.setItem('primeiroAcesso', 'true');
+// }
+window.location.href = '../index.html';
