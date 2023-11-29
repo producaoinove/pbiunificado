d92a38e436b1cd2f0b2b7ed6032576eda63fcf83
producaoinove <producao@inoveempresarial.com.br>
2023-11-29 15:08:49 -0300

    adcionando novas versoes

--- assets/script/scriptlogin.js
+++ assets/script/scriptlogin.js
@@ -4,9 +4,19 @@ function realizarLOGIN() {
         event.preventDefault();
         const username = form.username.value;
         const password = form.password.value;
+        const motivo = form.motivo.value;
+        let redirectUrl = "";
+
         if ((username === "higor.ruy" && password === "123456") || (username === "producao.inove" && password === "123456") || (username === "luiz.esquivel" && password === "123456") || (username === "jhenyfer.cordeiro" && password === "123456") || (username === "vendas.inove" && password === "123456") || (username === "back.office" && password === "123456")) {
             alert('SUCESSO NO LOGIN. \n !ATENÇÃO! MUDANÇAS NO TIPO VENDA - deverão seguir pelo Legado! \n PRESSIONE OK PARA CONTINUAR!');
-            window.location.href = "private/home.html";
+
+            if (motivo === "1") {
+                redirectUrl = "private/home1.html";
+            } else if (motivo === "2") {
+                redirectUrl = "private/home2.html";
+            }
+
+            window.location.href = redirectUrl;
         } else {
             alert('FRACASSO NO LOGIN. PRESSIONE OK PARA TENTAR NOVAMENTE!');
             window.location.href = "index.html";
